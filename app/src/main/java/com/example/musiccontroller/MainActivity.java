8c617110bcf14d16d73ba3b42627b2f5daa9caa4
aMiR <amir_bs720@hotmail.com>
2019-07-01 17:55:35 +0430

    removing comments

--- app/src/main/java/com/example/musiccontroller/MainActivity.java
+++ app/src/main/java/com/example/musiccontroller/MainActivity.java
@@ -5,22 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -57,42 +48,11 @@ public class MainActivity extends AppCompatActivity {
                                 AudioManager.STREAM_MUSIC,
                                 // Request permanent focus.
                                 AudioManager.AUDIOFOCUS_GAIN);
-
-
-
                     }
                 };
                 timer.schedule(task , delay);
-
             }
         });
-
-    }
-
-    public String[] getCurrentTime() {
-
-//        Calendar cal = Calendar.getInstance();
-//
-//        Date date = cal.getTime();
-//
-//        DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.);
-//
-//        String formattedDate=dateFormat.format(date);
-//
-//        Log.e("MyTime", formattedDate);
-////        System.out.println("Current time of the day using Calendar - 24 hour format: "+ formattedDate);
-//        Instant instant = Instant.now();
-//        Date date = new Date();
-//        date.setHours(date.getHours() + 8);
-//        SimpleDateFormat simpDate;
-//        simpDate = new SimpleDateFormat("kk:mm:ss");
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        String currentTime = dtf.format(now);
-        String[] time = currentTime.split(" ")[1].split(":");
-        return time;
-
     }
 
     @Override
