730541f766e506400a4cec87d2b80caadb1184a3
aMiR <amir_bs720@hotmail.com>
2019-07-02 16:19:54 +0430

    using thread

--- app/src/main/java/com/example/musiccontroller/MainActivity.java
+++ app/src/main/java/com/example/musiccontroller/MainActivity.java
@@ -6,17 +6,19 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    long threadId = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +27,32 @@ public class MainActivity extends AppCompatActivity {
         startTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                if (threadId != 0){
+//                    Thread thread = Thread.currentThread();
+//                    thread.interrupt();
+//                    threadId = 0;
+//                }
+
+                //Give you set of Threads
+                Set<Thread> setOfThread = Thread.getAllStackTraces().keySet();
+
+
+                //Iterate over set to find yours
+                for(Thread thread : setOfThread){
+                    if(thread.getId()==threadId){
+                        Log.e("interrupting" , "we're here with:"+Long.toString(thread.getId()));
+                        thread.interrupt();
+                        Log.e("is it working" ,Long.toString(threadId) + ":" + Boolean.toString(thread.isInterrupted()) + String.valueOf(Thread.interrupted()));
+                        threadId = 0;
+                        break;
+
+                    }
+                }
 
                 EditText timeInHourEditText = findViewById(R.id.time_in_hour);
                 EditText timeInMinEditText = findViewById(R.id.time_in_min);
+                Log.e("threadCount" , Integer.toString(Thread.activeCount()));
+
                 TextView timeRemainingTextView = findViewById(R.id.remaining_time);
                 int timeInHour =Integer.parseInt(timeInHourEditText.getText().toString());
                 int timeInMin =Integer.parseInt(timeInMinEditText.getText().toString());
@@ -35,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(getApplicationContext(),"Your music will pause in " + delay/60/1000 + " minutes", Toast.LENGTH_SHORT);
                 toast.show();
                 Thread thread = new Thread(new TimerThread(delay , timeRemainingTextView));
+                threadId = thread.getId();
+                Log.e("original id" , Long.toString(thread.getId()));
 
                 thread.start();
                 Timer timer = new Timer();
--- app/src/main/java/com/example/musiccontroller/TimerThread.java
+++ app/src/main/java/com/example/musiccontroller/TimerThread.java
@@ -1,30 +1,42 @@
 package com.example.musiccontroller;
 
+import android.util.Log;
 import android.widget.TextView;
 
 public class TimerThread implements Runnable {
 
     int delay;
+    long threadId;
     TextView textView;
+    boolean flag = true;
 
     public TimerThread(int delay , TextView textView) {
-        this.delay = delay/1000;
+        this.delay = delay / 1000;
         this.textView = textView;
     }
 
+
+
     @Override
     public void run() {
-        while(delay < 0){
+        while(delay > 0 && !Thread.interrupted()){
+            Log.e("current Thread Id" , Long.toString(Thread.currentThread().getId()) +":"+ Boolean.toString(Thread.currentThread().isInterrupted()) + ":" + Thread.interrupted());
+
+            if(Thread.currentThread().isInterrupted()){
+                Log.e("in Run", "about to break");
+                break;
+            }
+            delay -= 1;
             int hour = delay/3600;
             int minute = (delay - hour * 3600) / 60;
             int second = delay - hour * 3600 - minute * 60;
             textView.setText(hour + ":" + minute +":"+second);
             try {
+
                 Thread.sleep(1 * 1000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
-            delay -= 1;
         }
     }
 }
