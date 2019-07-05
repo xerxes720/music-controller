61157858deb4aa40c78cbd815f56546181e666b3
xerxes720 <amir_bs720@hotmail.com>
2019-07-06 02:45:38 +0430

    cleaning code

--- app/src/main/java/com/example/musiccontroller/MainActivity.java
+++ app/src/main/java/com/example/musiccontroller/MainActivity.java
@@ -2,27 +2,48 @@ package com.example.musiccontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accessibilityservice.GestureDescription;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     long threadId = 0;
+
+    ArrayList<String> GenerateMinutes(int maxNumber){
+        String[] result;
+        ArrayList<String> sb = new ArrayList<>();
+
+        for (int i = 0 ; i<=maxNumber; i++){
+            sb.add(Integer.toString(i));
+        }
+        return sb;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner hourSpinner = findViewById(R.id.hour_spinner);
+        Spinner minuteSpinner = findViewById(R.id.minute_spinner);
+//        hourSpinner.setOnItemClickListener(this);
         Button startTimerButton = findViewById(R.id.start_timer);
         startTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,8 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 final int delay = (timeInHour * 60 * 60 + timeInMin * 60)*1000;
                 Toast toast = Toast.makeText(getApplicationContext(),"Your music will pause in " + delay/60/1000 + " minutes", Toast.LENGTH_SHORT);
                 toast.show();
-                Thread thread2 = new Thread(new TimerThread(delay , timeRemainingTextView));
-                Thread thread = new Thread(new TimerThread(thread2 , delay , timeRemainingTextView));
+                Thread thread = new Thread(new TimerThread(delay , timeRemainingTextView));
                 threadId = thread.getId();
                 Log.e("original id" , Long.toString(thread.getId()));
 
@@ -76,5 +96,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
--- app/src/main/java/com/example/musiccontroller/TimerThread.java
+++ app/src/main/java/com/example/musiccontroller/TimerThread.java
@@ -16,42 +16,27 @@ public class TimerThread implements Runnable {
         this.textView = textView;
     }
 
-    public TimerThread(Thread thread ,int delay , TextView textView) {
-        this.delay = delay / 1000;
-        this.textView = textView;
-        this.thread = thread;
-    }
 
 
     @Override
     public void run() {
-        while(delay > 0 ){
-//            Log.e("current Thread Id" , Long.toString(Thread.currentThread().getId()) +":"+ Boolean.toString(Thread.currentThread().isInterrupted()) + ":" + Thread.interrupted());
+        while(delay > 0 ) {
 
-            if(Thread.interrupted()){
+            if (Thread.interrupted()) {
                 Log.e("in Run", "about to break");
                 break;
             }
             delay -= 1;
-            int hour = delay/3600;
+            int hour = delay / 3600;
             int minute = (delay - hour * 3600) / 60;
             int second = delay - hour * 3600 - minute * 60;
-            textView.setText(hour + ":" + minute +":"+second);
+            textView.setText(hour + ":" + minute + ":" + second);
 
-
-            if (Thread.interrupted()) {
-                try {
-                    Thread.sleep(1 * 1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }else{
-                try {
-                    Thread.sleep(1 * 1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Thread.currentThread().interrupt();
-                }
+            try {
+                Thread.sleep(1 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
